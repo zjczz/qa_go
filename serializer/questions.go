171379761d321fdfac93579673283b94da267d69
freedem <2227099513@qq.com>
2020-12-22 17:21:18 +0800

    添加首页问题列表接口

--- serializer/questions.go
+++ serializer/questions.go
@@ -8,9 +8,30 @@ type QuestionsData struct {
 	Questions []QuestionData `json:"questions"`
 }
 
+// 首页问题列表信息
+type QuestionsData struct {
+	Count     int             `json:"count"`
+	Questions []QuestionBrief `json:"questions"`
+}
+
+type QuestionBrief struct {
+	ID     uint         `json:"id"`
+	Title  string       `json:"title"`
+	Answer *AnswerBrief `json:"answer"`
+}
+
+// 单个回答信息
+type AnswerBrief struct {
+	ID          uint   `json:"id"`
+	Content     string `json:"content"`
+	Avatar      string `json:"avatar"`
+	Nickname    string `json:"nickname"`
+	Description string `json:"description"`
+}
+
 // 序列化热门问题列表
-func BuildHotQuestions(questions []model.Question) *QuestionsData {
-	questionsData := QuestionsData{}
+func BuildHotQuestions(questions []model.Question) *HotQuestionsData {
+	questionsData := HotQuestionsData{}
 	questionsData.Count = len(questions)
 	questionsData.Questions = make([]QuestionData, len(questions))
 	for index, question := range questions {
@@ -28,7 +49,39 @@ func BuildHotQuestions(questions []model.Question) *QuestionsData {
 	return &questionsData
 }
 
-// 问题列表响应信息
+// 序列化首页问题列表
+func BuildQuestions(questions []model.Question) *QuestionsData {
+	questionsData := QuestionsData{}
+	questionsData.Count = len(questions)
+	questionsData.Questions = make([]QuestionBrief, len(questions))
+	for index, question := range questions {
+
+		questionData := QuestionBrief{
+			ID:     question.ID,
+			Title:  question.Title,
+			Answer: nil,
+		}
+		if len(question.Answers) != 0 {
+			profile, _ := model.GetUserProfile(question.Answers[0].UserID)
+			answer := AnswerBrief{}
+			answer.ID = question.Answers[0].ID
+			answer.Content = question.Answers[0].Content
+			answer.Avatar = profile.Avatar
+			answer.Nickname = profile.Nickname
+			answer.Description = profile.Description
+			questionData.Answer = &answer
+		}
+		questionsData.Questions[index] = questionData
+	}
+	return &questionsData
+}
+
+// 热门问题列表响应信息
+type HotQuestionsResponse struct {
+	HotQuestionsData
+}
+
+// 首页问题列表响应信息
 type QuestionsResponse struct {
 	QuestionsData
 }
