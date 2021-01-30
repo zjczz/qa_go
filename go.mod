beb5cd136fa449c8c660060f1e5a72d9f9e873a2
freedem <2227099513@qq.com>
2021-01-30 13:38:11 +0800

    添加点赞时间排序功能，增加GetUserLikes，通过uid获取点赞列表

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/joho/godotenv v1.3.0
+	github.com/onsi/ginkgo v1.14.2 // indirect
+	github.com/onsi/gomega v1.10.4 // indirect
 	github.com/robfig/cron v1.2.0
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392
 	gorm.io/driver/mysql v1.0.3
--- model/like.go
+++ model/like.go
@@ -2,11 +2,13 @@ package model
 
 import (
 	"fmt"
+	"github.com/go-redis/redis"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"qa_go/cache"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // UserLike 用户点赞表
@@ -26,15 +28,55 @@ const (
 const (
 	AnswerLikeCount = "answer_like_count"
 	UserLikeRecord  = "user_like_record"
+	UserLikeAnswers = "user_like_answers"
 )
+
+// GetUserLike 获取用户uid对回答aid的点赞情况
+func GetUserLikes(uid uint) ([]uint, error) {
+	var likes []UserLike
+	err := DB.Where("user_id = ? and status = 1", uid).
+		Order("updated_at desc").Find(&likes).Error
+
+	// 从redis中按时间倒序获取缓存的aid，
+	// aid>0：说明用户对其点赞，aid<0：；说明用户对其取消点赞
+	key := fmt.Sprintf("%s:%d", UserLikeAnswers, uid)
+	var likesCache []int
+	err = cache.RedisClient.ZRevRange(key, 0, -1).ScanSlice(&likesCache)
+
+	var res []uint
+
+	// set记录aid是否有更新，
+	// 如果有更新则将数据放入res，并丢弃数据库数据
+	set := make(map[uint]struct{})
+	var member struct{}
+	for _, aid := range likesCache {
+		if aid > 0 {
+			res = append(res, uint(aid))
+			set[uint(aid)] = member
+		} else {
+			set[uint(-aid)] = member
+		}
+	}
+
+	for _, like := range likes {
+		aid := like.AnswerID
+		if _, ok := set[aid]; !ok {
+			res = append(res, aid)
+		}
+	}
+
+	return res, err
+}
 
 // GetUserLike 获取用户uid对回答aid的点赞情况
 func GetUserLike(uid uint, aid uint) (uint, error) {
 	// 首先从redis中获取，获取到直接返回，否则从数据库获取
 	key := fmt.Sprintf("%d:%d", uid, aid)
 	// 在redis中找到了
-	if res, err := cache.RedisClient.HGet(UserLikeRecord, key).Int(); err == nil {
-		return uint(res), err
+	if res, err := cache.RedisClient.HGet(UserLikeRecord, key).Result(); err == nil {
+		split := strings.Split(res, ":")
+		status, _ := strconv.Atoi(split[0])
+		return uint(status), err
 	}
 	// 在redis中没有找到，从数据库获取
 	var userLike UserLike
@@ -54,16 +96,20 @@ func AddUserLike(uid uint, aid uint, status uint) error {
 		return err
 	}
 	var incr int64 = 0
+	pipe := cache.RedisClient.TxPipeline()
 	// 根据前后的状态，判断点赞数的增减与否
+	keyAns := fmt.Sprintf("%s:%d", UserLikeAnswers, uid)
 	if (pre == NONE || pre == DOWN) && status == UP {
 		incr = 1
+		pipe.ZAdd(keyAns, redis.Z{Score: float64(time.Now().Unix()), Member: aid})
 	} else if pre == UP && (status == NONE || status == DOWN) {
 		incr = -1
+		pipe.ZRem(keyAns, aid)
 	}
-	pipe := cache.RedisClient.TxPipeline()
 	pipe.HIncrBy(AnswerLikeCount, strconv.Itoa(int(aid)), incr)
-	key := fmt.Sprintf("%d:%d", uid, aid)
-	pipe.HSet(UserLikeRecord, key, status)
+	keyRec := fmt.Sprintf("%d:%d", uid, aid)
+	pipe.HSet(UserLikeRecord, keyRec,
+		fmt.Sprintf("%d:%d", status, time.Now().Unix()))
 	_, err = pipe.Exec()
 	return err
 }
@@ -85,16 +131,21 @@ func SyncUserLikeRecord() {
 	for key, val := range data {
 		//fmt.Printf("%s\t%s\n", key, val)
 
-		split := strings.Split(key, ":")
-		uid, _ := strconv.Atoi(split[0])
-		aid, _ := strconv.Atoi(split[1])
-		status, _ := strconv.Atoi(val)
+		splitK := strings.Split(key, ":")
+		uid, _ := strconv.Atoi(splitK[0])
+		aid, _ := strconv.Atoi(splitK[1])
+
+		splitV := strings.Split(val, ":")
+		status, _ := strconv.Atoi(splitV[0])
+		updateTime, _ := strconv.ParseInt(splitV[1], 10, 64)
 
 		var userLike UserLike
 		userLike.UserID = uint(uid)
 		userLike.AnswerID = uint(aid)
 
 		row := DB.Where(&userLike).Find(&userLike).RowsAffected
+
+		userLike.UpdatedAt = time.Unix(updateTime, 0)
 
 		var err error
 		// 存在则更新，不存在则创建
@@ -112,6 +163,13 @@ func SyncUserLikeRecord() {
 
 	// 删除redis中的数据
 	cache.RedisClient.Del(UserLikeRecord)
+
+	// 匹配所有的UserLikeAnswers相关的key，将其删除
+	var keys []string
+	cache.RedisClient.Keys(fmt.Sprintf("%s*", UserLikeAnswers)).ScanSlice(&keys)
+	for _, key := range keys {
+		cache.RedisClient.Del(key)
+	}
 }
 
 // SyncUserLikeRecord 将redis中的回答点赞数量同步到数据库
@@ -124,6 +182,7 @@ func SyncAnswerLikeCount() {
 
 	for key, val := range data {
 		//fmt.Printf("%s\t%s\n", key, val)
+
 		id, _ := strconv.Atoi(key)
 		count, _ := strconv.Atoi(val)
 
